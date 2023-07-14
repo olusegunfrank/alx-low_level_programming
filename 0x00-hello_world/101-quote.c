aad3700a9392656db73f959d600d7a2728d0030b
Francis <francisolusegun20@gmail.com>
2023-07-13 18:50:53 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,9 @@
  *
  * Return: Always 1 (Success)
  */
-
+int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
-	int write(int filedes, const char *buf, unsigned int nbyte);
+	
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
