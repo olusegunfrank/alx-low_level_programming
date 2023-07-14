b8dfe75a43264f3f1d29cff035f299ec8d51e004
Francis <francisolusegun20@gmail.com>
2023-07-13 18:49:05 -0700

    commit quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 
 int main(void)
+	int write(int filedes, const char *buf, unsigned int nbyte);
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
