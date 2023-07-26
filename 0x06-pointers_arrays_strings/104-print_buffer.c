beb123b68204b8a61d01b1ec2360a68224cb7d6a
Francis <francisolusegun20@gmail.com>
2023-07-26 11:32:06 -0700

    pointer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * print_buffer - add 2 strings.
+ * @b: string1.
+ * @size: string2.
+ * Return: String with all letters in ROT13 base.
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	i = 0;
+	if (size <= 0)
+		putchar('\n');
+	else
+	{
+		while (i < size)
+		{
+			printf("%08x: ", i);
+			j = 0;
+			while (j < 10)
+			{
+				if (j % 2 == 0 && j > 0)
+					printf(" ");
+				if (j + i > size - 1)
+					printf("  ");
+				else
+					printf("%.2x", b[j + i]);
+				j++;
+			}
+			putchar(' ');
+			j = 0;
+			while (j < 10)
+			{
+				if (j + i > size - 1)
+					break;
+				if (b[j + i] >= ' ' && b[j + i] <= '~')
+					putchar(b[j + i]);
+				else
+					putchar('.');
+				j++;
+			}
+			putchar('\n');
+			i += 10;
+		}
+	}
+
+}
