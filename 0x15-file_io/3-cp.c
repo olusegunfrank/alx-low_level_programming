1213d3354fcd52e7a0671a5d376196c620729466
Francis <francisolusegun20@gmail.com>
2023-09-04 14:47:48 -0700

    add

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,85 +1,56 @@
 #include "main.h"
-
-
 /**
- * _err - function to check for error code
- * @stat: error code to be checked
- * Return: void
+ * main - C program that copied file content from one to another
+ * @argc: size of array argv and number of command line arguments
+ * @av: An  array  of size argc
+ * Return: Always successful
  */
-void _err(int stat, ...)
+int main(int argc, char *av[])
 {
-	va_list list;
+	int file1, file2, fileread, filewrite;
+	char buffer[1024];
 
-	va_start(list, stat);
-	if (stat == 97)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	else if (stat == 98)
+	file1 = open(av[1], O_RDONLY);
+	if (file1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file ");
-		dprintf(STDERR_FILENO, "%s\n", va_arg(list, char *));
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	else if (stat == 99)
+	file2  = open(av[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
+	if (file2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to ");
-		dprintf(STDERR_FILENO, "%s\n", va_arg(list, char *));
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-	else
+	do  {
+		fileread = read(file1, buffer, 1024);
+		if (fileread ==  -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+		filewrite = write(file2, buffer, fileread);
+		if (filewrite == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			exit(99);
+		}
+	} while (fileread == 1024);
+
+	if (close(file1) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd ");
-		dprintf(STDERR_FILENO, "%d\n", va_arg(list, int));
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1);
 		exit(100);
 	}
-	va_end(list);
-}
-
-/**
- * main - main function entry point
- * @argc: number of arguments passed
- * @argv: arguments being passed
- * Return: 98 read err, 99 write err, 100 close err, 0 sucess
- *
- */
-int main(int argc, char **argv)
-{
-	int input, output, input_r, output_w;
-	char *buff;
-
-	if (argc != 3)
-		_err(97);
-	if (argv[1] == NULL)
-		_err(98, argv[1]);
-	if (argv[2] == NULL)
-		_err(99, argv[2]);
-	input = open(argv[1], O_RDONLY);
-	if (input == -1)
-		_err(98, argv[1]);
-	output = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (output == -1)
-		_err(99, argv[2]);
-	buff = malloc(BUFFSIZE * sizeof(char));
-	if (buff == NULL)
-		return (1);
-	input_r = read(input, buff, BUFFSIZE);
-	if (input_r == -1)
-		_err(98, argv[1]);
-	while (input_r > 0)
+	if (close(file2) == -1)
 	{
-		output_w = write(output, buff, input_r);
-		if (output_w == -1)
-			_err(99, argv[2]);
-		input_r = read(input, buff, BUFFSIZE);
-		if (input_r == -1)
-			_err(98, argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2);
+		exit(100);
 	}
-	if (close(input) == -1)
-		_err(100, input);
-	if (close(output) == -1)
-		_err(100, output);
-	free(buff);
 	return (0);
 }
